33ad31bd00662353827146d24aa0291628a38a0e
poke646 <k2490262@gmail.com>
2020-03-31 15:00:47 +0900

    listMember 업데이트

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -2,10 +2,22 @@ package com.example.demo.dao;
 
 public class MemberDao {
 	
-	public void insertMember() {
-		
+	public void listMember() {
+		System.out.println("listMember, 회원정보를 수정하였습니다.");
+	}
+	
+	public void insertMember() {		
 		System.out.println("회원을 등록하였습니다.");
-			
+		System.out.println("회원 정보를 수정함.");
+	}
+
+	public void updateMember() {
+		System.out.println("회원의 정보를 고쳤습니다.");
+		System.out.println("회원의 정보를 수정함!!");
+	}
+
+	public void deleteMember() {
+		System.out.println("회원의 정보를 삭제함!");
 	}
 
 }
