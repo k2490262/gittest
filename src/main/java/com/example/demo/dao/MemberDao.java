578b53ff7cd4ad5e3d42db8dcda40a326b7717ae
sajh78 <redksa0708@gmail.com>
2020-03-31 15:10:23 +0900

    list,login 처리

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -2,6 +2,14 @@ package com.example.demo.dao;
 
 public class MemberDao {
 	
+	public void login() {
+		System.out.println("로그인했고요!");
+	}
+	
+	public void listMember() {
+		System.out.println("회원목록이요!!!");
+	}
+	
 	public void insertMember() {		
 		System.out.println("회원 정보를 수정함.");
 	}
