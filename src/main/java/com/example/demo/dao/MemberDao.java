ad0b5103340750bdd3e9b033328e93ddee4b6b21
sajh78 <redksa0708@gmail.com>
2020-03-31 14:56:42 +0900

    deleteMember까지 업뎃요

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -3,11 +3,15 @@ package com.example.demo.dao;
 public class MemberDao {
 	
 	public void insertMember() {		
-		System.out.println("회원을 등록하였습니다.");
+		System.out.println("회원 정보를 수정함.");
 	}
 	
 	public void updateMember() {
-		System.out.println("회원의 정보를 고쳤습니다.");
+		System.out.println("회원의 정보를 수정함!!");
+	}
+	
+	public void deleteMember() {
+		System.out.println("회원의 정보를 삭제함!");
 	}
 
 }
