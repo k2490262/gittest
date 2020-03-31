9da47ab6cf70dca2b729f22caaeb60ca7180275e
sajh78 <redksa0708@gmail.com>
2020-03-31 14:49:14 +0900

    updateMember완성

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -2,10 +2,12 @@ package com.example.demo.dao;
 
 public class MemberDao {
 	
-	public void insertMember() {
-		
+	public void insertMember() {		
 		System.out.println("회원을 등록하였습니다.");
-			
+	}
+	
+	public void updateMember() {
+		System.out.println("회원의 정보를 고쳤습니다.");
 	}
 
 }
